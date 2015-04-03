7f50aac5850d9c635011f4e4fc4862434786b370
Tommy Brown <thomasbrown73095@gmail.com>
2015-04-03 18:38:05 -0500

    Initial commit

---
+++ crowddj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  crowddj
+//
+//  Created by Tommy Brown on 4/3/15.
+//  Copyright (c) 2015 tommista. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ crowddj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  crowddj
+//
+//  Created by Tommy Brown on 4/3/15.
+//  Copyright (c) 2015 tommista. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
